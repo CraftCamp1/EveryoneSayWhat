de1b8d5d01e4c00046a0aec8ebbaa35a395bdcc7
CraftCamp <93895629+CraftCamp1@users.noreply.github.com>
2023-01-28 13:32:37 +0800

    Update Main.php

--- src/CraftCamp/EveryoneSayWhat/Main.php
+++ src/CraftCamp/EveryoneSayWhat/Main.php
@@ -11,39 +11,40 @@ use pocketmine\Server;
 class Main extends PluginBase {
 
     public function onCommand(CommandSender $sender, Command $command, string $label, array $args): bool {
-        switch ($command->getName()) {
-            case "everyonesaywhat":
-            case "esw":
-                if(empty($args)){
-                    $sender->sendMessage("Please provide a message to be broadcasted");
-                    return true;
-                }
-                $players = $this->getServer()->getOnlinePlayers();
-                if (empty($players)) {
-                    $sender->sendMessage("No players to execute command on");
-                    return true;
-                }
-                $message = implode(" ", $args);
-                $count = 0;
-                foreach ($players as $player) {
-                    if ($player === $sender) continue;
-                    if (!$player->hasPermission("everyonesaywhat.deny")) {
-                        $player->chat($message);
-                        $count++;
-                    }
-                }
-                if ($count === 0) {
-                    $sender->sendMessage("No players to execute command on");
-                }else{
-                    if($sender instanceof Player){
-                        $this->getServer()->getLogger()->info("[".$sender->getName()."] said: ".$message);
-                    }else{
-                        $this->getServer()->getLogger()->info("[Console] said: ".$message);
-                    }
-                }
-                return true;
-                break;
+        if (!$this->checkCommand($sender, $args)) {
+            return true;
         }
-        return false;
+        $message = implode(" ", $args);
+        $count = 0;
+        foreach ($this->getServer()->getOnlinePlayers() as $player) {
+            if ($player === $sender || $player->hasPermission("everyonesaywhat.deny")) {
+                continue;
+            }
+            $player->chat($message);
+            $count++;
+        }
+        if ($count === 0) {
+            $sender->sendMessage("No players to execute command on");
+        } else {
+            $this->logCommand($sender, $message);
+        }
+        return true;
+    }
+
+    private function checkCommand(CommandSender $sender, array $args): bool {
+        if (empty($args)) {
+            $sender->sendMessage("Please provide a message to be broadcasted");
+            return false;
+        }
+        if (empty($this->getServer()->getOnlinePlayers())) {
+            $sender->sendMessage("No players to execute command on");
+            return false;
+        }
+        return true;
+    }
+
+    private function logCommand(CommandSender $sender, string $message): void {
+        $name = $sender instanceof Player ? $sender->getName() : "Console";
+        $this->getServer()->getLogger()->info("[$name] said: $message");
     }
 }
