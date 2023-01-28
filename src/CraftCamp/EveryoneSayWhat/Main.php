46ef8c65f698ec8574a5c9e7bcdb7ef15e163882
CraftCamp <93895629+CraftCamp1@users.noreply.github.com>
2023-01-28 13:28:10 +0800

    Update Main.php

--- src/CraftCamp/EveryoneSayWhat/Main.php
+++ src/CraftCamp/EveryoneSayWhat/Main.php
@@ -4,7 +4,7 @@ namespace CraftCamp\EveryoneSayWhat;
 
 use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
-use pocketmine\Player;
+use pocketmine\player\Player;
 use pocketmine\plugin\PluginBase;
 use pocketmine\Server;
 
