bede98e3af49c65de53b212c50093beb0ea7c67a
CraftCamp <93895629+CraftCamp1@users.noreply.github.com>
2023-01-28 14:02:01 +0800

    Update Main.php

--- src/CraftCamp/EveryoneSayWhat/Main.php
+++ src/CraftCamp/EveryoneSayWhat/Main.php
@@ -11,27 +11,24 @@ use pocketmine\Server;
 
 class Main extends PluginBase {
 
-    public function onCommand(CommandSender $sender, Command $command, string $label, array $args): bool {
+        public function onCommand(CommandSender $sender, Command $command, string $label, array $args): bool {
         if (!$this->checkCommand($sender, $args)) {
             return true;
         }
         $message = implode(" ", $args);
-        $count = 0;
-        foreach ($this->getServer()->getOnlinePlayers() as $player) {
-            if ($player->hasPermission("everyonesaywhat.deny")) {
-                continue;
-            }
-            $player->chat($message);
-            $count++;
-        }
-        if ($count === 0) {
+        $players = array_filter($this->getServer()->getOnlinePlayers(), function ($player) {
+            return !$player->hasPermission("everyonesaywhat.deny");
+        });
+        if(count($players) === 0){
             $sender->sendMessage("No players to execute command on");
-        } else {
+        }else{
+            foreach($players as $player){
+                $player->chat($message);
+            }
             $this->logCommand($sender, $message);
         }
         return true;
     }
-
     private function checkCommand(CommandSender $sender, array $args): bool {
         if (empty($args)) {
             $sender->sendMessage("Please provide a message to be broadcasted");
@@ -48,7 +45,7 @@ class Main extends PluginBase {
         $name = $sender instanceof Player ? $sender->getName() : "Console";
         $this->getServer()->getLogger()->info("[$name] Used ESW: $message");
         foreach($this->getServer()->getOnlinePlayers() as $player){
-            if($player->hasPermission("everyonesaywhat.use") && $sender !== $player || $sender instanceof ConsoleCommandSender){
+            if($player->hasPermission("everyonesaywhat.use") && !$sender->hasPermission("everyonesaywhat.use") || $sender instanceof ConsoleCommandSender){
                 $player->sendMessage("[$name] Used ESW: $message");
             }
         }
